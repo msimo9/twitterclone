ae64d112195a1c1f5e2988efb43ff37b5c62dc85
msimo9 <matjaz.simonic@os-ajdovscina.si>
2022-03-14 18:25:45 +0100

    kinda tired on working on this app :D, will lay it off for now, maybe come back later

--- screens/ExploreScreen.js
+++ screens/ExploreScreen.js
@@ -1,14 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
 import React from 'react'
+import styles from '../styles/ExploreStyle'
+import HeaderProfilePicture from '../components/HeaderProfilePicture';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Header = () => {
+  return(
+    <View style={styles.header}>
+      <TextInput style={styles.input}/>
+    </View>
+  )
+}
+
+const Settings = () => {
+  return(
+    <View style={styles.settingsContainer}>
+      <Ionicons name={"settings-outline"} size={18} color={"#ffffff"} />
+    </View>
+  )
+}
 
 const ExploreScreen = () => {
   return (
-    <View>
-      <Text>ExploreScreen</Text>
+    <View style={styles.container}>
+      <HeaderProfilePicture />
+      <Header />
+      <Settings />
     </View>
   )
 }
 
 export default ExploreScreen
-
-const styles = StyleSheet.create({})
